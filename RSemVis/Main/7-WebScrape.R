4d57c5524a36db16cf08d66f389e318d78419359
Viktor Hansen <vikhan91@gmail.com>
2016-12-01 03:27:28 +0100

    ADD baby steps web scraper

--- RSemVis/Main/7-WebScrape.R
+++ RSemVis/Main/7-WebScrape.R
@@ -1,2 +1,12 @@
 library(rvest)
 
+foafOntology <- read_html("http://xmlns.com/foaf/spec/")
+foafOntology
+
+lego_movie <- read_html("http://www.imdb.com/title/tt1490017/")
+
+
+foafOntology %>%
+  html_node("strong span") %>%
+  html_text() %>%
+  as.numeric()
